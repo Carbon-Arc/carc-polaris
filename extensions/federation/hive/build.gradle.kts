1228abaf3b9b6cb3638814a7a7cc253e393f97ab
carc-prathyush-shankar <prathyush.shankar@carbonarc.co>
2025-11-07 15:00:04 -0800

    build: add hadoop-aws as runtime dep

--- extensions/federation/hive/build.gradle.kts
+++ extensions/federation/hive/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     exclude("com.sun.jersey", "jersey-servlet")
     exclude("io.dropwizard.metrics", "metrics-core")
   }
+  runtimeOnly("org.apache.hadoop:hadoop-aws:3.4.2")
 
   // CDI dependencies for runtime discovery
   implementation(libs.jakarta.enterprise.cdi.api)
